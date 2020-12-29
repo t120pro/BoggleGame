86db217f213f85ec734558633ccf4d4018513741
Tehilla <60326519+t120pro@users.noreply.github.com>
2020-12-29 00:58:46 -0500

    removed unnecessary global vars

--- Score.py
+++ Score.py
@@ -1,19 +1,25 @@
-class score:
+class Score:
 
-    __name = ""
-    __high_score = 0
-
-    def __init__(self, name, high_score):
-        __name = name
-        __high_score = high_score
+    def __init__(self, name, score):
+        self._name = name
+        self._score = int(score)
 
     def __str__(self):
-        return self.__name + " " + self.high_score
+        return self._name + "~" + str(self._score)
 
     def compare_to(self, other):
-        if self.__high_score < other.high_score:
+        if self._score < other._score:
             return -1
-        elif self.__high_score == other.high_score:
+        elif self._score == other._score:
             return 0
         else:
             return 1
+        
+    def get_score(self):
+        return self._score
+    def get_name(self):
+        return self._name
+    
+    def add_points(word):
+        if len(word) > 2:
+            self._score += len(word)

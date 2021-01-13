020bec74ed62c561c6688a76184616d1d4b27d8e
Tehilla <60326519+t120pro@users.noreply.github.com>
2021-01-12 20:54:02 -0500

    Update Board.py

--- Board.py
+++ Board.py
@@ -6,7 +6,24 @@ class Board():
         self.create_board()
         
     def create_board(self):
-        self._board = [[0 for x in range(5)] for y in range(4)]
-        for y in range(5):
-            for x in range(4):
+        self._board = [[0 for x in range(4)] for y in range(5)]
+        for y in range(4):
+            for x in range(5):
                 self._board[x][y] = Cube()
+            
+    def print_board(self):
+        print("______________\n")
+        for y in range(4):
+            for x in range(5):
+                print(self._board[x][y], end=" ")
+                if self._board[x][y] != "Qu":
+                    print(" ", end="")
+            if y != 3:
+                print("\n")
+        print("\n______________")
+            
+    def get_board(self):
+        return self._board
+    
+    def get_by_index(self, x, y):
+        return self._board[x][y]
